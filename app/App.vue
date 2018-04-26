ce79a3d6c9b5b1fb10a07111b54dda4aec84af79
Dewitt Buckingham <dj729@comcast.net>
2018-04-25 23:24:00 -0700

    Mockup Task Interface

--- app/App.vue
+++ app/App.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
-    <h2>Howdy Y'All Balls</h2>
-    <input 
-      v-model="todo" 
-      type="text" 
-      placeholder="To Do">
-    <span>{{ todo }}</span>
+    <h2>Add Task</h2>
+    <form @submit.prevent="addTodo">
+      <input 
+        v-model="todo" 
+        type="text" 
+        placeholder="Add new task"
+        name="todo">
+      <span>{{ todo }}</span>
+    </form>
+    <div class="listContainer">
+      <div class="activeTodos">
+        <h3>Doing</h3>
+        <ul>
+          <li>Take out the Trash <a href=""><i class="fa fa-check-circle"/></a>&nbsp;<a href=""><i class="fa fa-minus-circle"/></a></li>
+          <li>Take out the Trash <a href=""><i class="fa fa-check-circle"/></a>&nbsp;<a href=""><i class="fa fa-minus-circle"/></a></li>
+          <li>Take out the Trash <a href=""><i class="fa fa-check-circle"/></a>&nbsp;<a href=""><i class="fa fa-minus-circle"/></a></li>
+        </ul>
+      </div>
+      <div class="inactiveTodos">
+        <h3>To Do</h3>
+        <ul>
+          <li>Take out the Trash <a href=""><i class="fa fa-minus-circle"/></a></li>
+          <li>Take out the Trash <a href=""><i class="fa fa-minus-circle"/></a></li>
+          <li>Take out the Trash <a href=""><i class="fa fa-minus-circle"/></a></li>
+        </ul>
+      </div>
+      <div class="completedTodos">
+        <h3>Done</h3>
+        <ul>
+          <li>Take out the Trash <a href=""><i class="fa fa-plus-circle"/></a></li>
+          <li>Take out the Trash <a href=""><i class="fa fa-plus-circle"/></a></li>
+          <li>Take out the Trash <a href=""><i class="fa fa-plus-circle"/></a></li>
+        </ul>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -20,4 +50,42 @@ export default {
 </script>
 
 <style>
+@import 'https://fonts.googleapis.com/css?family=Exo+2';
+@import 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+
+body {
+  font-family: 'Exo 2', sans-serif;
+  background-color: #ccc;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+}
+
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+
+.listContainer > div {
+  background-color: #fff;
+  margin: 0.5rem 0;
+  padding: 0.5rem;
+  text-align: center;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 0.5rem;
+  font-size: 1.3rem;
+  background-color: #e0edf4;
+  border-left: 5px solid blue;
+  margin-bottom: 0.25rem;
+  color: navy;
+}
 </style>
