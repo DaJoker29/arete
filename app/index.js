9c592f2bb63898f243a7086e179b74ac9a055d8c
Dewitt Buckingham <dj729@comcast.net>
2018-04-26 18:43:41 -0700

    Style Prototype

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+String.prototype.titleCase =
+  String.prototype.titleCase ||
+  function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };
+
 new Vue({
   el: '#app',
   render: h => h(App),
